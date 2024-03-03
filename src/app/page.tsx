4e5990c16d211fad1931aa0529b9ee5682dd0b84
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-03-03 19:40:28 +0100

    chore: make signup and login route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to space</h1>
+      <Button>Click me</Button>
     </div>
-  );
+  )
 }
